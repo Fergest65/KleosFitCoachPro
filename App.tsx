e4ac19357d38140463134ca8fe462c05d4101f77
Rino Ferrari <rinoferrari@virgilio.it>
2026-05-27 09:21:45 +0200

    Minimal test app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'black', alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: 'lime', fontSize: 32, fontWeight: '900' }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#000',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text
+        style={{
+          color: '#B6FF00',
+          fontSize: 32,
+          fontWeight: '900',
+        }}>
         KLEOS OK
       </Text>
     </View>
